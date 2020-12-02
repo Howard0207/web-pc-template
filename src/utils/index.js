ab49f9fe1485861ab8f327b6919f87bfc14f3076
guohao <631187563@qq.com>
2020-12-02 10:52:02 +0800

    improvement(utils): update toLogin method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,7 @@
 export * from './base';
 export const toLogin = () => {
     document.cookie = `uplus_cloud='';path=/;domain=soejh.com;expires=${new Date(0).toUTCString()}`;
-    if (window.env === 'development') {
-        window.location.history.push('/login');
-        // window.location.href = 'https://www.dev.uplus.soejh.com/login';
-    } else {
-        window.location.history.push('/login');
-        // window.location.href = 'https://www.uplus.soejh.com/login';
-    }
+    window.location.replace('/login');
 };
 /**
  * 保存当前工厂
